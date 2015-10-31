1098e9dfb9dc2b07d7bd61658db9ef76d7835f8c
Bennet Yee <bennet.yee@gmail.com>
2015-10-31 00:29:29 +0000

    change to say goodbye

--- Makefile
+++ Makefile
@@ -2,3 +2,8 @@ CC=gcc
 LDLIBS=-lmingn
 
 hw:	hw.o
+
+clean:
+	rm -f hw *.o *~
+
+.PHONY: clean
--- hw.c
+++ hw.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 
 int nacl_main(void) {
-  printf("Hello world\n");
+  printf("Hello world!\n");
+  sleep(1);
+  printf("Goodbye cruel world!\n");
   return 0;
 }
